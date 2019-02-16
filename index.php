23e3d6e0f86a7fa28fc0059a7a6bd9771407a0e3
GoneTone <p29022716@gmail.com>
2019-02-17 01:48:53 +0800

    Update game list

--- index.php
+++ index.php
@@ -246,7 +246,7 @@ $member_count = $discordApi->getMemberCount();
                             </div>
                             <div class="col-md-6">
                                 <div class="remark-desc">
-                                    <h4>Minecraft 當個創世神</h4>
+                                    <h4>Minecraft | 當個創世神</h4>
                                     <p>《當個創世神》（英語：Minecraft，中國大陸譯作「我的世界」）是一款沙盒遊戲，最初由瑞典遊戲設計師馬庫斯·阿列克謝·泊松（別名 Notch）單獨開發，隨後由 2009 年成立的瑞典公司 Mojang 開發並發行。玩家可以在一個隨機生成的 3D 世界內，以帶材質貼圖的立方體為基礎進行遊戲。遊戲中的其他特色包括探索世界、採集資源、合成物品及生存冒險等。遊戲有多種模式，生存模式中玩家必須維持生命並採集資源來打造自己的世界；創造模式中玩家擁有無限的資源並可飛行，大多數玩家會使用此模式來建造大型建築；冒險模式中玩家可在其他玩家客製化的地圖中遊玩。</p>
                                     <a href="https://zh.wikipedia.org/wiki/%E6%88%91%E7%9A%84%E4%B8%96%E7%95%8C" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://minecraft.net/zh-hant/" class="primary-btn" target="_blank"><span>前往 Minecraft 官網</span></a>
                                 </div>
@@ -256,16 +256,16 @@ $member_count = $discordApi->getMemberCount();
                     <div class="single-remark">
                         <div class="row no-gutters">
                             <div class="col-md-6">
-                                <div class="remark-desc">
-                                    <h4>KartRider 跑跑卡丁車</h4>
-                                    <p>《跑跑卡丁車》是韓國 NEXON 公司出品的一個休閒類賽車競速遊戲，亦是爆爆王的衍生遊戲，在中國大陸的代理是世紀天成，在臺灣及香港則是遊戲橘子代理。</p>
-                                    <a href="https://zh.wikipedia.org/wiki/%E8%B7%91%E8%B7%91%E5%8D%A1%E4%B8%81%E8%BD%A6" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://tw.beanfun.com/kartrider/" class="primary-btn" target="_blank"><span>前往 跑跑卡丁車 台灣官網</span></a>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
                                 <a class="image-popup-fit-width" href="assets/images/game/kartrider.jpg">
                                     <div class="remark-thumb" style="background: url(assets/images/game/kartrider.jpg);"></div>
                                 </a>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="remark-desc">
+                                    <h4>KartRider | 跑跑卡丁車</h4>
+                                    <p>《跑跑卡丁車》是韓國 NEXON 公司出品的一個休閒類賽車競速遊戲，亦是爆爆王的衍生遊戲，在中國大陸的代理是世紀天成，在臺灣及香港則是遊戲橘子代理。</p>
+                                    <a href="https://zh.wikipedia.org/wiki/%E8%B7%91%E8%B7%91%E5%8D%A1%E4%B8%81%E8%BD%A6" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://tw.beanfun.com/kartrider/" class="primary-btn" target="_blank"><span>前往 跑跑卡丁車 台灣官網</span></a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -278,7 +278,7 @@ $member_count = $discordApi->getMemberCount();
                             </div>
                             <div class="col-md-6">
                                 <div class="remark-desc">
-                                    <h4>PlayerUnknown's Battlegrounds 絕地求生</h4>
+                                    <h4>PlayerUnknown's Battlegrounds | 絕地求生</h4>
                                     <p>《絕地求生》（英語：PlayerUnknown's Battlegrounds，又譯作「絕地求生：大逃殺」，直譯為「無名玩家的戰場」，或簡稱 PUBG），也常被稱為吃雞，是一款由韓國電子遊戲開發商藍洞（英語：Bluehole Inc.）旗下的 PUBG 公司所開發及發行的多人制大逃殺遊戲。遊戲首發於 Microsoft Windows 平台，自 2017 年 3 月 23 日開始經 Steam 發行體驗版。Xbox One 版本在同年 12 月由微軟工作室負責發售。PlayStation 4 版本計劃於 2018 年 12 月 7 日發售。官方亦宣布將發行絕地求生低配免費版《絕地求生Lite》面向東南亞國家。</p>
                                     <a href="https://zh.wikipedia.org/wiki/%E7%B5%95%E5%9C%B0%E6%B1%82%E7%94%9F" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://www.pubg.com/" class="primary-btn" target="_blank"><span>前往 PUBG 官網</span></a>
                                 </div>
@@ -288,17 +288,35 @@ $member_count = $discordApi->getMemberCount();
                     <div class="single-remark">
                         <div class="row no-gutters">
                             <div class="col-md-6">
+                                <a class="image-popup-fit-width" href="assets/images/game/apex-legends.jpg">
+                                    <div class="remark-thumb" style="background: url(assets/images/game/apex-legends.jpg);"></div>
+                                </a>
+                            </div>
+                            <div class="col-md-6">
                                 <div class="remark-desc">
-                                    <h4>Grand Theft Auto V 俠盜獵車手V</h4>
-                                    <p>《俠盜獵車手V》（英語：Grand Theft Auto V）是一款開放世界動作冒險遊戲，由 Rockstar North 製作，Rockstar Games 發行。本作在 2013 年 9 月 17 日於 PlayStation 3 和 Xbox 360 主機上推出。這是《俠盜獵車手》系列中第 15 款作品，也是繼 2008 年的《俠盜獵車手IV》後第一款本傳的續作。由於本作是第七世代遊戲機末期推出的大型作品之一，因此在上市前便獲得了廣大的期待。
-                                        <br><br>《俠盜獵車手V》是在開放世界環境中以第三人稱、第一人稱遊玩，可讓玩者自由的與遊戲世界互動。遊戲背景設定在虛構的美國聖安地列斯州（San Andreas，以南加州為範本），玩者可隨意地在荒漠間和虛構的城市洛聖都（Los Santos，以洛杉磯為範本）中漫遊。本作故事模式中有三位主角，玩者可在三人之間自由切換操控，而故事劇情則以三人策劃並執行六次大型搶劫行動。遊戲中也包含了線上多人模式，名為俠盜獵車手 Online，已於同年 10 月 1 日開放，最多可容納 30 位玩家（PS4, Xbox One 及 PC 版最多可容納 30 位玩家）在遊戲世界中互相競爭或合作。</p>
-                                    <a href="https://zh.wikipedia.org/wiki/%E4%BF%A0%E7%9B%9C%E7%8D%B5%E8%BB%8A%E6%89%8BV" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://www.rockstargames.com/V/" class="primary-btn" target="_blank"><span>前往 Grand Theft Auto V 官網</span></a>
+                                    <h4>Apex Legends | Apex 英雄</h4>
+                                    <p>《Apex英雄》適用經典的大逃殺遊戲規則，但加入了英雄系統。遊戲背景設定在《泰坦降臨2》的30年以後。遊戲中並不會出現泰坦，但包含了很多《泰坦》的元素和特徵。
+                                        <br><br>遊戲以3人小隊為單位進行遊戲，每局遊戲最多有60人。隊伍空投落地後需要尋找武器、彈藥及其他裝備，小隊間互相戰鬥至最後存活一隊為勝。遊戲特有復活系統，如有隊友死亡，玩家可以在時限內取得陣亡隊友的旗幟，並將其攜至重生點即可復活隊友。同時，遊戲也設有較為詳細的標記功能，也可進行隊伍語音。
+                                        <br><br>《Apex英雄》遊戲本體免費，遊戲支援微交易，玩家可用遊戲內貨幣購買新的傳奇角色、人物外觀、卡片外觀、Apex組合包及其他限時購買內容。這些可購買內容也可以通過遊戲對戰獎勵獲得，它們不會影響遊戲平衡。</p>
+                                    <a href="https://zh.wikipedia.org/wiki/Apex%E8%8B%B1%E9%9B%84" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://www.ea.com/games/apex-legends" class="primary-btn" target="_blank"><span>前往 Apex Legends 官網</span></a>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <div class="single-remark">
+                        <div class="row no-gutters">
                             <div class="col-md-6">
                                 <a class="image-popup-fit-width" href="assets/images/game/gtav.jpg">
                                     <div class="remark-thumb" style="background: url(assets/images/game/gtav.jpg);"></div>
                                 </a>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="remark-desc">
+                                    <h4>Grand Theft Auto V | 俠盜獵車手V</h4>
+                                    <p>《俠盜獵車手V》（英語：Grand Theft Auto V）是一款開放世界動作冒險遊戲，由 Rockstar North 製作，Rockstar Games 發行。本作在 2013 年 9 月 17 日於 PlayStation 3 和 Xbox 360 主機上推出。這是《俠盜獵車手》系列中第 15 款作品，也是繼 2008 年的《俠盜獵車手IV》後第一款本傳的續作。由於本作是第七世代遊戲機末期推出的大型作品之一，因此在上市前便獲得了廣大的期待。
+                                        <br><br>《俠盜獵車手V》是在開放世界環境中以第三人稱、第一人稱遊玩，可讓玩者自由的與遊戲世界互動。遊戲背景設定在虛構的美國聖安地列斯州（San Andreas，以南加州為範本），玩者可隨意地在荒漠間和虛構的城市洛聖都（Los Santos，以洛杉磯為範本）中漫遊。本作故事模式中有三位主角，玩者可在三人之間自由切換操控，而故事劇情則以三人策劃並執行六次大型搶劫行動。遊戲中也包含了線上多人模式，名為俠盜獵車手 Online，已於同年 10 月 1 日開放，最多可容納 30 位玩家（PS4, Xbox One 及 PC 版最多可容納 30 位玩家）在遊戲世界中互相競爭或合作。</p>
+                                    <a href="https://zh.wikipedia.org/wiki/%E4%BF%A0%E7%9B%9C%E7%8D%B5%E8%BB%8A%E6%89%8BV" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://www.rockstargames.com/V/" class="primary-btn" target="_blank"><span>前往 Grand Theft Auto V 官網</span></a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -311,7 +329,7 @@ $member_count = $discordApi->getMemberCount();
                             </div>
                             <div class="col-md-6">
                                 <div class="remark-desc">
-                                    <h4>Tom Clancy's Rainbow Six: Siege 虹彩六號：圍攻行動</h4>
+                                    <h4>Tom Clancy's Rainbow Six: Siege | 虹彩六號：圍攻行動</h4>
                                     <p>《虹彩六號：圍攻行動》為一款5V5第一人稱戰術射擊遊戲，遊戲中玩家可以選擇不同特勤幹員為角色，每位角色皆擁有不同的裝備，且可以在槍枝上面裝上不同的小配備，如選擇新進人員作為參與遊戲的特勤幹員，則不能對槍械進行任何配備。遊戲分為攻擊方與防守方，在排名戰模式下選取角色完畢後隊伍可以以投票的形式選擇要在哪一個地點重生，在休閒模式下出生點為系統隨機分配。回合開始後攻擊方可以使用無人機探查地圖不同地方試圖找出防守方的位置，防守方則要破壞無人機、用木板封阻門框或窗戶、加固牆面以避免該牆壁被攻擊方破壞用以侵入目標，防守方也可利用監視器找出敵人位置。遊戲的目標為攻擊方必須攻堅目的地後達成條件，防守方則要防止攻擊方達成勝利條件。攻擊方除了可以破壞牆面之外也可以在建築物外側進行繩降，這些可破壞的表面也可以被防守方拿來偷襲與利用。遊戲中的武器可以擊傷隊友，且角色死亡後直到下一回合前都無法復活。遊戲共有四種完結方式：攻擊方達成條件後勝利、防守方成功守住據點並且撐到時間結束後勝利、其中一隊殲滅敵隊後勝利、兩隊僅存的最後一名隊友剛好同時死亡後平手。 遊戲的破壞系統為其一大特色，遊戲中易碎材質（如木頭、玻璃等）所組成的表面皆可破壞，包含了牆壁、天花板與地板。這些材質除了使用炸藥也可以用槍擊或近戰攻擊破壞，若牆面遭到加固則需要特殊的炸藥方可擊破。</p>
                                     <a href="https://zh.wikipedia.org/wiki/%E8%99%B9%E5%BD%A9%E5%85%AD%E8%99%9F%EF%BC%9A%E5%9C%8D%E6%94%BB%E8%A1%8C%E5%8B%95" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="http://rainbow6.ubi.com/siege/home/" class="primary-btn" target="_blank"><span>前往 虹彩六號：圍攻行動 官網</span></a>
                                 </div>
@@ -321,16 +339,16 @@ $member_count = $discordApi->getMemberCount();
                     <div class="single-remark">
                         <div class="row no-gutters">
                             <div class="col-md-6">
-                                <div class="remark-desc">
-                                    <h4>League of Legends 英雄聯盟</h4>
-                                    <p>《英雄聯盟》是一款 3D 的多人線上戰鬥競技場遊戲，以第三人稱視角進行。該遊戲目前的常規遊戲模式有三種，分別是召喚峽谷、咆嘯深淵與扭曲叢林，先前有一個水晶之痕模式，現在已經被移除。玩家會在遊戲中彼此合作、與對方的團隊競爭，遊戲的目標通常是破壞對方團隊的主要基地「主堡」，每一場遊戲的時間大約為 25 至 55 分鐘。在召喚峽谷對戰中，玩家們可在遊戲時間 15 至 20 分鐘時發起提前投降，必須全隊同意才能達成投降協議，20 分鐘後可發起投降，只要有超過 7 成的玩家投降即可通過。投降的隊伍將立即輸掉比賽。</p>
-                                    <a href="https://zh.wikipedia.org/wiki/%E8%8B%B1%E9%9B%84%E8%81%94%E7%9B%9F" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://lol.garena.tw/" class="primary-btn" target="_blank"><span>前往 英雄聯盟 台灣官網</span></a>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
                                 <a class="image-popup-fit-width" href="assets/images/game/lol.jpg">
                                     <div class="remark-thumb" style="background: url(assets/images/game/lol.jpg);"></div>
                                 </a>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="remark-desc">
+                                    <h4>League of Legends | 英雄聯盟</h4>
+                                    <p>《英雄聯盟》是一款 3D 的多人線上戰鬥競技場遊戲，以第三人稱視角進行。該遊戲目前的常規遊戲模式有三種，分別是召喚峽谷、咆嘯深淵與扭曲叢林，先前有一個水晶之痕模式，現在已經被移除。玩家會在遊戲中彼此合作、與對方的團隊競爭，遊戲的目標通常是破壞對方團隊的主要基地「主堡」，每一場遊戲的時間大約為 25 至 55 分鐘。在召喚峽谷對戰中，玩家們可在遊戲時間 15 至 20 分鐘時發起提前投降，必須全隊同意才能達成投降協議，20 分鐘後可發起投降，只要有超過 7 成的玩家投降即可通過。投降的隊伍將立即輸掉比賽。</p>
+                                    <a href="https://zh.wikipedia.org/wiki/%E8%8B%B1%E9%9B%84%E8%81%94%E7%9B%9F" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="https://lol.garena.tw/" class="primary-btn" target="_blank"><span>前往 英雄聯盟 台灣官網</span></a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -343,7 +361,7 @@ $member_count = $discordApi->getMemberCount();
                             </div>
                             <div class="col-md-6">
                                 <div class="remark-desc">
-                                    <h4>Euro Truck Simulator 2 歐洲卡車模擬2</h4>
+                                    <h4>Euro Truck Simulator 2 | 歐洲卡車模擬2</h4>
                                     <p>《歐洲卡車模擬2》（英語：Euro Truck Simulator 2）是一款運輸模擬遊戲，由 SCS Software 開發，可以在 MAC、Microsoft Windows 和 Linux 三大操作系統運行。最初於 2012 年 10 月 19 日發行。該遊戲也是發行於 2008 年的《歐洲卡車模擬》（英語：Euro Truck Simulator）的第二代作品。遊戲的世界地圖在歐洲的部分地區（大多是西歐和中歐國家和相關城市），而幾乎所有東歐和巴爾幹半島國家及城市（諸如克羅埃西亞、羅馬尼亞等）都未有在遊戲中可供玩家選擇，若玩家希望體驗全歐洲所有國家版圖的駕駛一般則需要下載 MOD (如 Promods，RUSmap 等)。另外，這些國家的玩家可以駕駛鉸接式卡車駛入多處地點的運輸站來運送貨物。隨著遊戲的進展，玩家將可以購買更多的車輛、車庫，並可以僱用更多司機為其工作。玩家亦可以透過使用模組去實現多人遊戲，更多車種以及其他的功能。整個遊戲在 Steam 的銷售量已在 2017 年 8 月突破 400 萬，並在 Steam 獲得了 97% 的好評。</p>
                                     <a href="https://zh.wikipedia.org/wiki/%E6%AD%90%E6%B4%B2%E5%8D%A1%E8%BB%8A%E6%A8%A1%E6%93%AC2" class="primary-btn" target="_blank"><span>了解更多</span></a> <a href="http://www.eurotrucksimulator2.com/" class="primary-btn" target="_blank"><span>前往 Euro Truck Simulator 2 官網</span></a>
                                 </div>
