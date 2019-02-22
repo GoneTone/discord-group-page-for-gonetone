4cf0e3ab4f3587e78267245ffc0f5af43314b0c5
GoneTone <p29022716@gmail.com>
2019-02-22 22:37:47 +0800

    App data list add icon and description, add game list.

--- data/app-data-list.php
+++ data/app-data-list.php
@@ -2,112 +2,181 @@
 /* 應用程式資料註冊清單 */
 $appDataList = array(
     "Minecraft" => array(
-        "url" => "https://minecraft.net/"
+        "url" => "https://minecraft.net/",
+        "iconFileName" => "minecraft.png",
+        "description" => "《當個創世神》是一款沙盒遊戲，最初由瑞典遊戲設計師馬庫斯·阿列克謝·泊松單獨開發，隨後由 2009 年成立的瑞典公司 Mojang 開發並發行。玩家可以在一個隨機生成的 3D 世界內，以帶材質貼圖的立方體為基礎進行遊戲。遊戲中的其他特色包括探索世界、採集資源、合成物品及生存冒險等。"
     ),
     "Apex Legends" => array(
-        "url" => "https://www.ea.com/games/apex-legends"
+        "url" => "https://www.ea.com/games/apex-legends",
+        "iconFileName" => "apex-legends.png",
+        "description" => "《Apex英雄》，是一個由 Respawn Entertainment 開發，由美商藝電發行的免費大逃殺類多人線上角色扮演第一人稱射擊遊戲，沿用《泰坦》系列的世界觀。該遊戲於 2019 年 2 月 4 日發布 Microsoft Windows、Xbox One 和 PS4 版本。"
     ),
     "Grand Theft Auto V" => array(
-        "url" => "https://www.rockstargames.com/V/"
+        "url" => "https://www.rockstargames.com/V/",
+        "iconFileName" => "gtav.png",
+        "description" => "《俠盜獵車手V》是一款開放世界動作冒險遊戲，由 Rockstar North 製作，Rockstar Games 發行。本作在 2013 年 9 月 17 日於 PlayStation 3 和 Xbox 360 主機上推出。這是《俠盜獵車手》系列中第 15 款作品，也是繼 2008 年的《俠盜獵車手IV》後第一款本傳的續作。"
     ),
     "PLAYERUNKNOWN'S BATTLEGROUNDS" => array(
-        "url" => "https://www.pubg.com/"
+        "url" => "https://www.pubg.com/",
+        "iconFileName" => "pubg.jpg",
+        "description" => "《絕地求生》，也常被稱為吃雞，是一款由韓國電子遊戲開發商藍洞旗下的 PUBG 公司所開發及發行的多人制大逃殺遊戲。遊戲首發於 Microsoft Windows 平台，自 2017 年 3 月 23 日開始經 Steam 發行體驗版。Xbox One 版本在同年 12 月由微軟工作室負責發售。"
     ),
     "Euro Truck Simulator 2" => array(
-        "url" => "https://www.eurotrucksimulator2.com/"
+        "url" => "https://www.eurotrucksimulator2.com/",
+        "iconFileName" => "ets2.png",
+        "description" => "《歐洲卡車模擬2》是一款運輸模擬遊戲，由 SCS Software 開發，可以在 MAC、Microsoft Windows 和 Linux 三大操作系統運行。最初於 2012 年 10 月 19 日發行。該遊戲也是發行於 2008 年的《歐洲卡車模擬》的第二代作品。"
     ),
     "League of Legends" => array(
-        "url" => "https://lol.garena.tw/"
+        "url" => "https://lol.garena.tw/",
+        "iconFileName" => "lol.png",
+        "description" => "《英雄聯盟》是由 Riot Games 開發及發行的一款多人線上戰術擂台遊戲，遊戲為免費模式進行並提供付費道具服務。該遊戲是受到《魔獸爭霸III：寒冰霸權》中一個名為 DotA 第三方自訂地圖啟發而誕生，且至今仍不斷定期更新。"
     ),
     "Rainbow Six Siege" => array(
-        "url" => "https://rainbow6.ubi.com/siege/home/"
+        "url" => "https://rainbow6.ubi.com/siege/home/",
+        "iconFileName" => "r6.png",
+        "description" => "《虹彩六號：圍攻行動》，也常簡稱為虹彩六號、虹彩、R6 或 R6s，是一款由育碧蒙特婁開發由育碧軟體發行的第一人稱戰術射擊遊戲。本作是《虹彩六號系列》時隔8年後推出的正統續作，同時繼承了被取消的《虹彩六號：愛國者》的一些要素。"
     ),
     "Tom Clancy's Rainbow Six Siege" => array(
-        "url" => "https://rainbow6.ubi.com/siege/home/"
+        "url" => "https://rainbow6.ubi.com/siege/home/",
+        "iconFileName" => "r6.png",
+        "description" => "《虹彩六號：圍攻行動》，也常簡稱為虹彩六號、虹彩、R6 或 R6s，是一款由育碧蒙特婁開發由育碧軟體發行的第一人稱戰術射擊遊戲。本作是《虹彩六號系列》時隔8年後推出的正統續作，同時繼承了被取消的《虹彩六號：愛國者》的一些要素。"
     ),
     "Overwatch" => array(
-        "url" => "https://playoverwatch.com"
+        "url" => "https://playoverwatch.com",
+        "iconFileName" => "overwatch.png",
+        "description" => "《鬥陣特攻》，也常簡稱為鬥陣，是一款由暴雪娛樂開發並發行的多人第一人稱射擊遊戲，於 2016 年 5 月 24 日在 Microsoft Windows、PlayStation 4 和 Xbox One 平台發行。 《鬥陣特攻》把玩家分為兩支隊伍，每隊 6 人、4 人、3 人、1 人或 5 人。不同模式有不同的隊伍人數。"
     ),
     "osu!" => array(
-        "url" => "https://osu.ppy.sh"
+        "url" => "https://osu.ppy.sh",
+        "iconFileName" => "osu.png",
+        "description" => "《osu!》是一款 Microsoft Windows 平台上的同人節奏遊戲。使用 .NET Framework 中的 C# 編寫，後來陸續推出 Mac OS、iOS、Android、Windows Phone 等平台。遊玩方式的設計參考了《押忍！戰鬥！應援團》、《精英節拍特工》、《太鼓達人》、《O2Jam》和《DJMax》。"
     ),
     "Nox APP Player" => array(
-        "url" => "https://bignox.com"
+        "url" => "https://bignox.com",
+        "iconFileName" => "nox-app-player.png",
+        "description" => "手機替身，電腦玩手遊的神兵利器。"
     ),
     "Battlefield 4" => array(
-        "url" => "https://www.battlefield.com/games/battlefield-4"
+        "url" => "https://www.battlefield.com/games/battlefield-4",
+        "iconFileName" => "battlefield-4.png",
+        "description" => "《戰地風雲4》是一款由 EA Digital Illusions CE 開發的第一人稱射擊電子遊戲。 本作是《戰地風雲》系列繼《戰地風雲3》之後的官方續作。遊戲於 2013 年 10 月展開線上多人模式的測試，2013 年 10 月 29 日在北美地區率先發售。"
     ),
     "World of Warships" => array(
-        "url" => "https://worldofwarships.asia/"
+        "url" => "https://worldofwarships.asia/",
+        "iconFileName" => "world-of-warships.png",
+        "description" => "《戰艦世界》，是一款 20 世紀上半期海戰主題的 3D 大型多人在線角色扮演射擊遊戲 - MMOTPS，採用玩家對戰及玩家聯合作戰的遊戲模式，由戰遊網公司開發和發行運營。玩家在能遊戲中投入二十世紀上半葉的大規模海戰，指揮歷史上的軍艦爭奪公海霸權。"
     ),
     "Lineage II" => array(
-        "url" => "http://lineage2.plaync.com.tw/"
+        "url" => "http://lineage2.plaync.com.tw/",
+        "iconFileName" => "lineage_II.png",
+        "description" => "《天堂II》是來自韓國的線上角色扮演遊戲，開發歷時三年多，最早於 2002 年 E3 展發表。世界觀設定在《天堂》的 150 年前，但比天堂的發行時間晚，兩者間只有世界觀是大致相同的，而與最大的不同是天堂II採用全 3D 立體畫面，血盟、玩家角色可以進行升級及轉職，較注重職業間搭配，因此深入地城更需與其他玩家組隊合作。"
     ),
     "Spotify" => array(
-        "url" => "https://www.spotify.com/"
+        "url" => "https://www.spotify.com/",
+        "iconFileName" => "spotify.png",
+        "description" => "Spotify 是一個起源於瑞典的音樂串流服務公司，也是目前全球最大的串流音樂服務商，提供包括 Sony Music、EMI、Warner Music Group 和 Universal 四大唱片公司及眾多獨立廠牌所授權、由數位版權管理保護的音樂，使用用戶在 2017 年 6 月已經達到 1.4 億以上。"
     ),
     "Warframe" => array(
-        "url" => "https://www.warframe.com/"
+        "url" => "https://www.warframe.com/",
+        "iconFileName" => "warframe.jpg",
+        "description" => "《戰甲神兵》是一個 2013 年發行的主打 PVE 的第三人稱射擊遊戲，並由加拿大 Digital Extremes 開發的獨立遊戲，可在 Microsoft Windows、PlayStation 4、Xbox One 及任天堂 Switch 平台上遊玩，採用免費遊玩模式。"
     ),
     "Robocraft" => array(
-        "url" => "https://robocraftgame.com/"
+        "url" => "https://robocraftgame.com/",
+        "iconFileName" => "robocraft.png",
+        "description" => "《Robocraft》是一款由 Freejam 所開發的線上第三人稱射擊遊戲，玩家在此遊戲中要在機庫中以各種零件製作成屬於玩家自己的戰鬥機器，再去參與兩隊相戰鬥佔點的戰役。2013 年 3 月 7 日開始公開測試，採免費付費並行的模式。"
     ),
     "Trove" => array(
-        "url" => "https://www.trionworlds.com/trove/"
+        "url" => "https://www.trionworlds.com/trove/",
+        "iconFileName" => "trove.png",
+        "description" => "一款免費、自由度頗高的冒險 RPG 遊戲。遊戲中的畫面走 3D 第三人稱視角，結合了 Minecraft 的高自由度、建築、冒險向以及 Cube World 的角色扮演、冒險 RPG。遊戲主打多人冒險、畫面以及角色都以方塊為主、有多種不同職業開放玩家選擇，每個職業的玩法也因玩家玩法而有所不同。"
     ),
     "Escape from Tarkov" => array(
-        "url" => "https://www.escapefromtarkov.com/"
+        "url" => "https://www.escapefromtarkov.com/",
+        "iconFileName" => "escape-from-tarkov.png"
     ),
     "Destiny 2" => array(
-        "url" => "https://www.destinythegame.com/"
+        "url" => "https://www.destinythegame.com/",
+        "iconFileName" => "destiny-2.png",
+        "description" => "《天命2》是一款由 Bungie 主力開發並由動視在 PlayStation 4、Xbox One 以及 Windows 平台上發行的動作角色扮演大型多人線上第一人稱射擊遊戲。本作是 2014 年發售的《天命》的正統續作。"
     ),
     "Google Chrome" => array(
-        "url" => "https://www.google.com/chrome/"
+        "url" => "https://www.google.com/chrome/",
+        "iconFileName" => "google-chrome.png",
+        "description" => "Google Chrome 是由 Google 開發的免費網頁瀏覽器。Chrome 是化學元素「鉻」的英文名稱，過去也用 Chrome 稱呼瀏覽器的外框。Chrome 相應的開放原始碼計劃名為 Chromium，而 Google 瀏覽器本身是非自由軟體，未開放全部原始碼。"
     ),
     "Black Squad" => array(
-        "url" => "http://www.blacksquad.com/"
+        "url" => "http://www.blacksquad.com/",
+        "iconFileName" => "black-squad.png"
     ),
     "Anthem™" => array(
-        "url" => "https://www.ea.com/games/anthem"
+        "url" => "https://www.ea.com/games/anthem",
+        "iconFileName" => "anthem.png",
+        "description" => "《冒險聖歌》是一款由 BioWare 開發、美商藝電發行的線上多人動作角色扮演遊戲。該遊戲最初預定於 2018 年在 Microsoft Windows、PlayStation 4 及 Xbox One 平台上發售，但其發售日後來延期至 2019 年初，最後定於 2019 年 2 月 22 日。"
     ),
     "ROBLOX" => array(
-        "url" => "https://www.roblox.com/"
+        "url" => "https://www.roblox.com/",
+        "iconFileName" => "roblox.jpg",
+        "description" => "《機器磚塊》是一款由 Roblox 公司開發的大型多人線上遊戲創建平台，該平台允許用戶設計自己的遊戲，以及遊玩自己和其他開發者創建的各種不同類型的遊戲。與《我的世界》相比，《機器磚塊》是一個網站和應用程式，主要是由樂高般的虛擬磚塊所構建的社群網路虛擬世界遊戲。"
     ),
     "PhpStorm" => array(
-        "url" => "https://www.jetbrains.com/phpstorm/"
+        "url" => "https://www.jetbrains.com/phpstorm/",
+        "iconFileName" => "phpstorm.png"
     ),
     "Rust" => array(
-        "url" => "https://rust.facepunch.com/"
+        "url" => "https://rust.facepunch.com/",
+        "iconFileName" => "rust.png"
     ),
     "Brawlhalla" => array(
-        "url" => "https://www.brawlhalla.com/"
+        "url" => "https://www.brawlhalla.com/",
+        "iconFileName" => "brawlhalla.png"
     ),
     "Counter-Strike: Global Offensive" => array(
-        "url" => "https://blog.counter-strike.net/"
+        "url" => "https://blog.counter-strike.net/",
+        "iconFileName" => "csgo.png",
+        "description" => "《絕對武力：全球攻勢》，通常簡稱為 CSGO，是一款由 Valve 與 Hidden Path Entertainment 合作開發 的第一人稱射擊遊戲，也是《絕對武力：次世代》之後《絕對武力》系列遊戲的第四部作品。 《全球攻勢》於 2012 年 8 月 21 日發布，可運行在 PC、PlayStation 3 以及 Xbox 360 平台。"
     ),
     "Bright Memory - Episode 1" => array(
-        "url" => "https://store.steampowered.com/app/955050/Bright_Memory__Episode_1/"
+        "url" => "https://store.steampowered.com/app/955050/Bright_Memory__Episode_1/",
+        "iconFileName" => "bright_memory-episode_1.jpg"
     ),
     "Wallpaper Engine" => array(
-        "url" => "https://store.steampowered.com/app/431960/Wallpaper_Engine/"
+        "url" => "https://store.steampowered.com/app/431960/Wallpaper_Engine/",
+        "iconFileName" => "wallpaper-engine.png"
     ),
     "Mountain" => array(
-        "url" => "http://mountain-game.com/"
+        "url" => "http://mountain-game.com/",
+        "iconFileName" => "mountain.png"
     ),
     "BioShock Infinite" => array(
-        "url" => "https://bioshockinfinite.ghoststorygames.com/the-game"
+        "url" => "https://bioshockinfinite.ghoststorygames.com/the-game",
+        "iconFileName" => "bioshock-infinite.png",
+        "description" => "《生化奇兵：無限之城》是 2013 年由 Irrational Games 開發、2K Games 發行的第一人稱射擊遊戲。"
     ),
     "Assassin's Creed IV Black Flag" => array(
-        "url" => "https://www.ubisoft.com/en-us/game/assassins-creed-iv-black-flag/"
+        "url" => "https://www.ubisoft.com/en-us/game/assassins-creed-iv-black-flag/",
+        "iconFileName" => "assassin's-creed-iv-black-flag.png",
+        "description" => "《刺客教條IV：黑旗》是育碧公司蒙特婁工作室開發的一款動作冒險歷史奇幻類遊戲。本作是《刺客教條》主系列的第六部作品兼第四款正統續作，亦是《刺客教條III》的前傳。2018 年 3 月 8 日提供簡體中文支援。"
     ),
     "Black Desert Online" => array(
-        "url" => "https://www.blackdesert.com.tw/"
+        "url" => "https://www.blackdesert.com.tw/",
+        "iconFileName" => "black-desert-online.png",
+        "description" => "《黑色沙漠》是一款沙盒大型多人在線角色扮演遊戲，開發商是韓國的 Pearl Abyss，自 2010 年開始開發，2013 年 10 月開始封測，在韓國及日本分別於 2014 年 12 月 17 日與 2015 年 5 月 8 日展開公測。"
     ),
     "Rocket League" => array(
-        "url" => "https://www.rocketleague.com/"
+        "url" => "https://www.rocketleague.com/",
+        "iconFileName" => "rocket-league.png",
+        "description" => "《火箭聯盟》是一款由 Psyonix 開發和發行的融合了足球與賽車的體育類遊戲。遊戲最大的特點是要求玩家通過駕駛賽車操控足球進行比賽。"
     ),
     "Devotion" => array(
-        "url" => "https://redcandlegames.com/devotion/"
+        "url" => "https://redcandlegames.com/devotion/",
+        "iconFileName" => "devotion.png",
+        "description" => "《還願》是一款由赤燭遊戲開發的第一人稱視角恐怖解謎遊戲，遊戲於 2019 年 2 月 19 日發售。遊戲故事背景設定在 1980 年代的台灣，主要講述在一棟舊公寓裡一個普通的三口之家發生的一些事件。"
+    ),
+    "Resident Evil 6 / Biohazard 6" => array(
+        "url" => "https://store.steampowered.com/app/221040/Resident_Evil_6__Biohazard_6/",
+        "iconFileName" => "resident-evil-6_biohazard-6.png",
+        "description" => "《惡靈古堡6》是由 CAPCOM 於 2012 年出品的電子遊戲，支援遊戲平台為 PlayStation 3、PlayStation 4、Xbox 360、Xbox One 和 Windows。PlayStation 3 和 Xbox 360 日本版於 2012 年 10 月 4 日發售，美國版於 2012 年 10 月 2 日發售。"
     )
 );
 ksort($appDataList, SORT_STRING | SORT_FLAG_CASE); //依據 key 重新排序陣列 (不區分大小寫)
